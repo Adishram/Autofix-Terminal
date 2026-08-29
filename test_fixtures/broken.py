71b6208d9b0e8467d2b2ba27e12b601dcb6ad100
Adish Ram <adishram006@gmail.com>
2026-08-29 11:40:25 +0530

    Delete test_fixtures directory

--- test_fixtures/broken.py
+++
@@ -1,5 +0,0 @@
-# A deliberately broken Python script for testing autofix
-Inp = input("Enter a tuple : ")
-for i in input:
-    print i
-print(Hello World)
--- test_fixtures/piped_test.py
+++
@@ -1,3 +0,0 @@
-tup = input("Enter a tuple: ")
-print("You entered:", tup)
-print(tup[100]) # This will cause an IndexError
